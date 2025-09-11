9aa6f583fdc2f75ad756a890fa1ae03adfee4a73
AngeloHerreraG <angelo.herrera.galaz@gmail.com>
2025-09-11 19:34:26 -0300

    base

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,25 +4,36 @@ import './App.css'
 interface User {
     id: number
     name: string
+    password: string
 }
 
 function App() {
-    const [users, setUsers] = useState<User[]>([])
+    const [patients, setPatients] = useState<User[]>([])
+    const [doctores, setDoctors] = useState<User[]>([])
 
-
-    fetch('http://localhost:9002/users')
+    fetch('http://localhost:9002/patients')
         .then(res => res.json())
-        .then(data => setUsers(data))
+        .then(data => setPatients(data))
         .catch(err => console.error(err))
 
+    fetch('http://localhost:9002/doctors')
+        .then(res => res.json())
+        .then(data => setDoctors(data))
+        .catch(err => console.error(err))
 
     return (
         <div>
             <h1>Hello World!</h1>
-            <h2>Usuarios desde el backend:</h2>
+            <h2>Pacientes desde el backend:</h2>
             <ul>
-                {users.map(user => (
-                    <li key={user.id}>{user.name}</li>
+                {patients.map(patient => (
+                    <li key={patient.id}>{patient.name}, {patient.password}</li>
+                ))}
+            </ul>
+            <h2>Doctores desde el backend:</h2>
+            <ul>
+                {doctores.map(doctor => (
+                    <li key={doctor.id}>{doctor.name}, {doctor.password}</li>
                 ))}
             </ul>
         </div>
